3fc6425a780c5d70656a9d4a31ff80e71d0d6605
rudrakshtumane <rudrakshtumane100@gmail.com>
2024-09-12 10:06:14 +0530

    updated productController

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -4,27 +4,32 @@ const Category = require("../model/categoryModel");
 async function createProduct(req, res) {
   try {
     const { name, category, price, description, inStock, quantity } = req.body;
-   
+
+    // Validate required fields
+    if (!name || !category || !price || !description || !quantity) {
+      return res.status(400).json({ msg: "All fields are required" });
+    }
 
     // Check if the product already exists (optional)
     let product = await Product.findOne({ name });
     if (product) {
       return res.status(400).json({ msg: "Product already exists" });
     }
+
     // Create a new product instance
     product = new Product({
       name,
       category,
       price,
       description,
-      inStock,
+      inStock, // Handle as boolean directly
       quantity,
       createdBy: req.user.id,
     });
 
     // Save the product to the database
     await product.save();
-    res.status(201).json(product);
+    res.status(201).json("Product added successfully");
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
@@ -38,9 +43,9 @@ async function updateProduct(req, res) {
       runValidators: true,
     });
     if (!product) {
-      return res.status(404).send();
+      return res.status(404).send("Product not found");
     }
-    res.status(200).send(product);
+    res.status(200).send("Product Edited Successfully");
   } catch (error) {
     res.status(500).send(error);
   }
@@ -50,7 +55,7 @@ async function deleteProduct(req, res) {
   try {
     const deletedProduct = await Product.findByIdAndDelete(req.params.id);
     if (!deletedProduct) {
-      return res.status(404).send();
+      return res.status(404).send("Product not found");
     }
     res.status(200).send({msg: "Product Deleted Successfully" });
   } catch (error) {
@@ -58,7 +63,7 @@ async function deleteProduct(req, res) {
   }
 }
 
-async function getAllProduct(re, res) {
+async function getAllProduct(req, res) {
   try {
     const products = await Product.find();
      // Modify each person object to include fullName, profileImage, and other necessary fields
